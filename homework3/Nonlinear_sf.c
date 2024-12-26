beb1b01c91eb66873cc0ccdedee131eb23fed033
QingyiRen <126511977+QingyiRen@users.noreply.github.com>
2024-12-26 08:09:01 +0100

    Add files via upload

---
+++ homework3/Nonlinear_sf.c
@@ -0,0 +1,13 @@
+/*
+ * Generated S-function Target for model Nonlinear. 
+ * This source file provides access to the generated S-function target
+ * for other models.
+ *
+ * Created: Sun May 16 15:22:56 2021
+ */
+
+#include "Nonlinear_sf.h"
+#include "Nonlinear_sfcn_rtw\Nonlinear_sf.c"
+#include "Nonlinear_sfcn_rtw\Nonlinear_sf_data.c"
+
+
